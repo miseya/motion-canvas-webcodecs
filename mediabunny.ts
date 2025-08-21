1af64b9b1e5a60ea9720459d2790153191f5a04d
Seya <31957516+sglkc@users.noreply.github.com>
2025-08-21 22:22:16 +0700

    add mediabunny exporter

---
+++ mediabunny.ts
@@ -0,0 +1,103 @@
+/// <reference lib="DOM" />
+
+import type { ExporterClass, Logger, RendererSettings } from '@motion-canvas/core/lib/app'
+import { makePlugin } from '@motion-canvas/core/lib/plugin'
+import { ObjectMetaField, type Project } from '@motion-canvas/core'
+import { Exporter } from '@motion-canvas/core/lib/app'
+import { Output, Mp4OutputFormat, BufferTarget, CanvasSource, QUALITY_HIGH } from 'mediabunny'
+
+export class WebCodecsExport implements Exporter {
+  public static readonly id = 'motion-canvas-webcodecs-exporter';
+  public static readonly displayName = 'WebCodecs';
+
+  public static meta() {
+    return new ObjectMetaField(this.name, {})
+  }
+
+  public static async create(
+    project: Project,
+    settings: RendererSettings,
+  ): Promise<WebCodecsExport> {
+    return new WebCodecsExport(project.logger, settings);
+  }
+
+  public constructor(
+    private readonly logger: Logger,
+    private readonly settings: RendererSettings,
+  ) { }
+
+  public myCanvas?: HTMLCanvasElement
+  public canvasCtx?: CanvasRenderingContext2D
+  public canvasSource?: CanvasSource
+  public output?: Output<Mp4OutputFormat, BufferTarget>
+  public frameTime?: number
+
+  public async start() {
+    this.myCanvas = document.createElement('canvas')
+    this.canvasCtx = this.myCanvas.getContext('2d')
+
+    this.canvasSource = new CanvasSource(this.myCanvas, {
+      codec: 'av1',
+      bitrate: QUALITY_HIGH,
+    })
+
+    this.output = new Output({
+      format: new Mp4OutputFormat({ fastStart: 'fragmented' }),
+      target: new BufferTarget(),
+    })
+
+    this.output.addVideoTrack(this.canvasSource, {
+      frameRate: this.settings.fps
+    })
+
+    this.frameTime = 1 / this.settings.fps
+
+    await this.output.start()
+    console.log('started export')
+  }
+
+  public async handleFrame(
+    canvas: HTMLCanvasElement,
+    frame: number,
+    sceneFrame: number,
+    sceneName: string,
+    signal: AbortSignal,
+  ) {
+    if (signal.aborted) {
+      await this.output.cancel()
+      return
+    }
+
+    this.canvasCtx.drawImage(canvas, 0, 0)
+    this.canvasSource.add(this.frameTime, this.frameTime)
+    // this.canvasCtx.save()
+  }
+
+  public async stop() {
+    if (this.output.state !== 'canceled')
+      await this.output.finalize()
+
+    const blob = new Blob([this.output.target.buffer], { type: 'video/mp4' })
+    const url = URL.createObjectURL(blob)
+    const a = document.createElement('a')
+
+    a.href = url
+    a.download = `${this.settings.name}.mp4`
+    a.click()
+
+    setTimeout(() => {
+      URL.revokeObjectURL(url)
+      a.remove()
+      console.log('stoped export')
+    }, 1)
+  }
+}
+
+const WebCodecs = makePlugin({
+  name: 'WebCodecs-plugin',
+  exporters(): ExporterClass[] {
+    return [WebCodecsExport]
+  },
+})
+
+export default WebCodecs
