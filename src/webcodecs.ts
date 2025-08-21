6fa4ab196865b121f2f2401ce5c9f75d079f0887
Seya <31957516+sglkc@users.noreply.github.com>
2025-08-22 00:25:20 +0700

    feat(WIP): add audio support

--- src/webcodecs.ts
+++ src/webcodecs.ts
@@ -2,7 +2,7 @@ import type { ExporterClass, Logger, RendererSettings } from '@motion-canvas/cor
 import { makePlugin } from '@motion-canvas/core/lib/plugin'
 import { BoolMetaField, EnumMetaField, NumberMetaField, ObjectMetaField, ValueOf, Project } from '@motion-canvas/core'
 import { Exporter } from '@motion-canvas/core/lib/app'
-import { Output, Mp4OutputFormat, BufferTarget, CanvasSource } from 'mediabunny'
+import { Output, Mp4OutputFormat, BufferTarget, AudioBufferSource, CanvasSource } from 'mediabunny'
 import * as mb from 'mediabunny'
 
 type WebCodecsExportOptions = ValueOf<ReturnType<typeof WebCodecsExporter.meta>>
@@ -12,34 +12,61 @@ class WebCodecsExporter implements Exporter {
   public static readonly displayName = 'WebCodecs'
 
   public static meta() {
-    const videoCodec = new EnumMetaField<mb.VideoCodec>(
-      'video codec',
-      mb.VIDEO_CODECS.map((codec) => ({ text: codec, value: codec })),
-      'av1'
-    )
-
-    const quality = new EnumMetaField('quality', [
+    const qualityEnum = [
       { text: 'very high', value: mb.QUALITY_VERY_HIGH },
       { text: 'high', value: mb.QUALITY_HIGH },
       { text: 'medium', value: mb.QUALITY_MEDIUM },
       { text: 'low', value: mb.QUALITY_LOW },
       { text: 'very low', value: mb.QUALITY_VERY_LOW },
       { text: 'custom', value: 0 }
-    ], mb.QUALITY_HIGH)
+    ]
 
-    const bitrate = new NumberMetaField('bitrate', 0)
+    const videoCodec = new EnumMetaField<mb.VideoCodec>(
+      'video codec',
+      mb.VIDEO_CODECS.map((codec) => ({ text: codec, value: codec })),
+      'av1'
+    )
+
+    const videoQuality = new EnumMetaField('video quality', qualityEnum, mb.QUALITY_HIGH)
+
+    const videoBitrate = new NumberMetaField('video bitrate', 0)
+      .describe('in bits, obviously')
+      .disable(true)
+
+    const includeAudio = new BoolMetaField('include audio', true)
+
+    const audioCodec = new EnumMetaField<mb.AudioCodec>(
+      'audio codec',
+      mb.AUDIO_CODECS.map((codec) => ({ text: codec, value: codec })),
+      'opus'
+    )
+
+    const audioQuality = new EnumMetaField('audio quality', qualityEnum, mb.QUALITY_HIGH)
+
+    const audioBitrate = new NumberMetaField('audio bitrate', 24000)
       .describe('in bits, obviously')
       .disable(true)
 
     const renderOnAbort = new BoolMetaField('render on abort', true)
 
-    // define dynamic options
-    quality.onChanged.subscribe((v) => bitrate.disable(v !== 0))
+    videoQuality.onChanged.subscribe((v) => videoBitrate.disable(v !== 0))
+
+    includeAudio.onChanged.subscribe((v) => {
+      audioCodec.disable(!v)
+      audioQuality.disable(!v)
+      audioBitrate.disable(audioQuality.get() !== 0)
+    })
+
+    audioQuality.onChanged.subscribe((v) => audioBitrate.disable(v !== 0))
 
     return new ObjectMetaField(this.displayName, {
       videoCodec,
-      quality,
-      bitrate,
+      videoQuality,
+      videoBitrate,
+      includeAudio,
+      audioCodec,
+      audioQuality,
+      audioBitrate,
       renderOnAbort,
     })
   }
@@ -48,21 +75,24 @@ class WebCodecsExporter implements Exporter {
     project: Project,
     settings: RendererSettings,
   ): Promise<WebCodecsExporter> {
-    return new WebCodecsExporter(project.logger, settings)
+    return new WebCodecsExporter(project, settings)
   }
 
+  private readonly logger: Logger
   private readonly options: WebCodecsExportOptions
 
   public constructor(
-    private readonly logger: Logger,
+    private readonly project: Project,
     private readonly settings: RendererSettings,
   ) {
+    this.logger = project.logger
     this.options = settings.exporter.options as WebCodecsExportOptions
   }
 
   public myCanvas?: HTMLCanvasElement
   public canvasCtx?: CanvasRenderingContext2D
   public canvasSource?: CanvasSource
+  public audioSource?: AudioBufferSource
   public output?: Output<Mp4OutputFormat, BufferTarget>
   public frameDuration: number = 0
   public frameStart: number = 0
@@ -78,7 +108,7 @@ class WebCodecsExporter implements Exporter {
 
     this.canvasSource = new CanvasSource(this.myCanvas, {
       codec: this.options.videoCodec,
-      bitrate: this.options.bitrate || this.options.quality,
+      bitrate: this.options.videoBitrate || this.options.videoQuality,
     })
 
     this.output = new Output({
@@ -89,6 +119,15 @@ class WebCodecsExporter implements Exporter {
     this.output.addVideoTrack(this.canvasSource, {
       frameRate: this.settings.fps
     })
+
+    if (this.options.includeAudio) {
+      this.audioSource = new AudioBufferSource({
+        codec: this.options.audioCodec,
+        bitrate: this.options.audioBitrate || this.options.audioQuality,
+      })
+
+      this.output.addAudioTrack(this.audioSource)
+    }
 
     this.frameDuration = 1 / this.settings.fps
     this.frameStart = this.settings.range[0] * this.settings.fps
@@ -112,10 +151,7 @@ class WebCodecsExporter implements Exporter {
       return
     }
 
-    if (signal.aborted) {
-      if (this.options.renderOnAbort) await this.output.cancel()
-      return
-    }
+    if (signal.aborted) return
 
     const timestampInSecs = (frame - this.frameStart) / this.settings.fps
 
@@ -129,10 +165,22 @@ class WebCodecsExporter implements Exporter {
       return
     }
 
-    this.logger.info('Finalizing render...')
+    if (!this.options.renderOnAbort && this.output.state === 'canceled') return
 
-    if (this.output.state !== 'canceled')
-      await this.output.finalize()
+    if (this.options.includeAudio && this.project.audio && this.audioSource) {
+      this.logger.info('Including audio...')
+
+      // TODO: trim based on time
+      const context = new AudioContext()
+      const res = await fetch(this.project.audio!)
+      const buffer = await res.arrayBuffer()
+      const audio = await context.decodeAudioData(buffer)
+
+      await this.audioSource.add(audio)
+    }
+
+    this.logger.info('Finalizing render...')
+    await this.output.finalize()
 
     if (!this.output.target.buffer) {
       this.logger.error('Output buffer is lost after finalizing somehow')
